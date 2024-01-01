4ef98979bff83326f6c61842997bb6c9ad7eefb9
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-01-01 16:57:15 +1100

    updated

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function ChatPage() {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Chat Page</title>
+      </Head>
+      <h1>Welcome to the chatId page</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>MoeGPT</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-    </div>
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!!user && <Link href="/api/auth/logout">Logout</Link>}
+          {!user && (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
